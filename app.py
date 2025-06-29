6ebb5bb441bd50c35e16f0fc59037d37f1290ed7
TheRingoflife <94365037+TheRingoflife@users.noreply.github.com>
2025-06-29 17:44:15 +0800

    app.py

--- app.py
+++ app.py
@@ -132,7 +132,6 @@ if st.sidebar.button("🧮 Predict"):
     st.pyplot(draw_hsr_star_plot(predicted_label))
 
     # === 概率表 ===
-    st.write("Model classes:", model.classes_)
     st.subheader("📊 Probability Table")
     true_classes = model.classes_
     hsr_labels = [label_map.get(cls, f"Class {cls}") for cls in true_classes]
